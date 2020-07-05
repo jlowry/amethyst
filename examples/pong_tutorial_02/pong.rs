238d8efcb3d045636f469140c111dab55147fe99
Nathan Stocks <cleancut@github.com>
2020-07-05 14:51:16 -0600

    Adjust translations so sprites render. Clear to black.
    Through trial and error I discovered two adjustments that would cause
    paddles to start rendering.  1) The camera needed to be shifted in the
    opposite x and y directions. 2) The paddle sprites needed to have
    negative z values.  This second problem was especially irritating, as I
    can't figure out _why_ it needs to be done. I just stumbled onto it.
    
    I set the screen to black as well. The rest of the changes are just
    rustfmt.

--- examples/pong_tutorial_02/pong.rs
+++ examples/pong_tutorial_02/pong.rs
@@ -1,8 +1,6 @@
 use amethyst::{
     assets::{AssetStorage, Handle, Loader},
-    core::{
-        transform::{LocalToWorld, Translation},
-    },
+    core::transform::{LocalToWorld, Translation},
     prelude::*,
     renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
 };
@@ -61,7 +59,6 @@ fn load_sprite_sheet(resources: &mut Resources) -> Handle<SpriteSheet> {
             .get::<Loader>()
             .expect("Could not get Loader resource");
 
-
         let texture_storage = resources.get::<AssetStorage<Texture>>().unwrap();
         loader.load(
             "texture/pong_spritesheet.png",
@@ -85,8 +82,8 @@ fn load_sprite_sheet(resources: &mut Resources) -> Handle<SpriteSheet> {
 /// Initialise the camera.
 fn initialise_camera(world: &mut World) {
     // Setup camera in a way that our screen covers whole arena and (0, 0) is in the bottom left.
-    let translation = Translation::new(ARENA_WIDTH * 0.5, ARENA_HEIGHT * 0.5, 1.0);
-    
+    let translation = Translation::new(-ARENA_WIDTH * 0.5, -ARENA_HEIGHT * 0.5, 0.0);
+
     world.insert(
         (),
         vec![(
@@ -99,11 +96,10 @@ fn initialise_camera(world: &mut World) {
 
 /// Initialises one paddle on the left, and one paddle on the right.
 fn initialise_paddles(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
-
     // Correctly position the paddles.
     let y = ARENA_HEIGHT / 2.0;
-    let left_translation = Translation::new(PADDLE_WIDTH * 0.5, y, 0.0);
-    let right_translation = Translation::new(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, 0.0);
+    let left_translation = Translation::new(PADDLE_WIDTH * 0.5, y, -1.0);
+    let right_translation = Translation::new(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, -1.0);
 
     // Assign the sprites for the paddles
     let sprite_render = SpriteRender {
